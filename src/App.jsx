0c16a326b0131bed696fcecb49466af98c906da3
Aidimor <jvr.unity@gmail.com>
2026-08-28 11:49:50 -0600

    28-08
    -Solved issue (first try)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import './App.css'
-import NavBar from './navbar'
-import Projects from "./projectsButtons"
-import MainInfo from "./mainInfo"
 import Layout from "./portfolioLayout"
 
 const LayoutOn = true;
-const NavbarOn = false;
-const ProjectsOn = false;
-const MainInfoOn = false;
+
+
 
 
 function App() {
   return (
     <>
       {LayoutOn && <Layout />}
-      {NavbarOn && <NavBar />}
-      {MainInfoOn && <MainInfo />}
-      {ProjectsOn && <Projects />}
+     
+      
   
       <main style={{ padding: '20px' }}>
   
