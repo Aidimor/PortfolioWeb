90c3af05fc6ceed74040061976739c311f6b3e8c
Aidimor <jvr.unity@gmail.com>
2026-08-28 11:06:31 -0600

    28-08

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,22 @@ import './App.css'
 import NavBar from './navbar'
 import Projects from "./projectsButtons"
 import MainInfo from "./mainInfo"
-import ProjectsInfo from "./projectsInfo"
 import Layout from "./portfolioLayout"
 
 const LayoutOn = true;
 const NavbarOn = false;
 const ProjectsOn = false;
 const MainInfoOn = false;
-const ProjectsInfoOn = false;
 
 
 function App() {
   return (
     <>
-    {LayoutOn && <Layout />}
+      {LayoutOn && <Layout />}
       {NavbarOn && <NavBar />}
-        {MainInfoOn && <MainInfo />}
-            {ProjectsInfoOn && <ProjectsInfo />}
+      {MainInfoOn && <MainInfo />}
       {ProjectsOn && <Projects />}
   
-    
       <main style={{ padding: '20px' }}>
   
       </main>
